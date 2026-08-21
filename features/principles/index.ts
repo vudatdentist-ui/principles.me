674ad6b63a6d5a871f48db07b3fdc4f19a605e85
Vũ Đạt <vudat.dentist@gmail.com>
2026-08-21 21:27:43 +0700

    V2-204: export principle candidate model

---
+++ features/principles/index.ts
@@ -0,0 +1,8 @@
+export { collectPrincipleCandidate, normalizePrincipleLesson } from "./candidate";
+export type {
+  CollectPrincipleCandidateInput,
+  InsufficientPrincipleCandidateSignalError,
+  PrincipleCandidate,
+  PrincipleCandidateConfidence,
+  PrincipleCandidateSignal,
+} from "./candidate";
